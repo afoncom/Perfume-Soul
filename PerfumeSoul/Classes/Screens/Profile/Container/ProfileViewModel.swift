776624494927af13b1e67bc190ecce42b524622c
afoncom <afon.com12@gmail.com>
2026-05-22 17:34:25 +0300

    Created knowledgeModule in perfumery

--- PerfumeSoul/Classes/Screens/Profile/Container/ProfileViewModel.swift
+++ PerfumeSoul/Classes/Screens/Profile/Container/ProfileViewModel.swift
@@ -12,4 +12,75 @@ import Observation
 @Observable final class ProfileViewModel {
     var profile: Profile?
     var isShowingDeleteProfileAlert = false
+    var totalCorrectQuizAnswers = 18
+
+    var perfumeExpertiseLevel: PerfumeExpertiseLevel {
+        PerfumeExpertiseLevel.level(for: totalCorrectQuizAnswers)
+    }
+
+    var answersToNextLevel: Int? {
+        perfumeExpertiseLevel.nextLevelStart.map { max($0 - totalCorrectQuizAnswers, 0) }
+    }
+
+    var perfumeExpertiseProgress: Double {
+        let lowerBound = perfumeExpertiseLevel.range.lowerBound
+        let upperBound = perfumeExpertiseLevel.nextLevelStart ?? perfumeExpertiseLevel.range.upperBound
+
+        guard upperBound > lowerBound else { return 1 }
+        let progress = Double(totalCorrectQuizAnswers - lowerBound) / Double(upperBound - lowerBound)
+        return min(max(progress, 0), 1)
+    }
+}
+
+enum PerfumeExpertiseLevel {
+    case novice
+    case noteExplorer
+    case accordExpert
+    case fragranceAnalyst
+    case perfumer
+
+    var range: ClosedRange<Int> {
+        switch self {
+        case .novice:
+            return 0...9
+        case .noteExplorer:
+            return 10...24
+        case .accordExpert:
+            return 25...49
+        case .fragranceAnalyst:
+            return 50...99
+        case .perfumer:
+            return 100...Int.max
+        }
+    }
+
+    var nextLevelStart: Int? {
+        switch self {
+        case .novice:
+            return 10
+        case .noteExplorer:
+            return 25
+        case .accordExpert:
+            return 50
+        case .fragranceAnalyst:
+            return 100
+        case .perfumer:
+            return nil
+        }
+    }
+
+    static func level(for correctAnswers: Int) -> PerfumeExpertiseLevel {
+        switch correctAnswers {
+        case ..<10:
+            return .novice
+        case 10..<25:
+            return .noteExplorer
+        case 25..<50:
+            return .accordExpert
+        case 50..<100:
+            return .fragranceAnalyst
+        default:
+            return .perfumer
+        }
+    }
 }
