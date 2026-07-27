85a65d61109fdc79846887279f1e9b9b3e559f09
afoncom <afon.com12@gmail.com>
2026-07-27 14:10:00 +0200

    Remove Unused Zodiac Longitude Mapping

--- PerfumeSoul/Classes/Services/ProfileCalculation/Astronomy/ZodiacSign.swift
+++ PerfumeSoul/Classes/Services/ProfileCalculation/Astronomy/ZodiacSign.swift
@@ -21,25 +21,6 @@ enum ZodiacSign: String, CaseIterable, Codable, Equatable, Hashable {
     case aquarius
     case pisces
 
-    init(longitude: Double) {
-        let normalizedLongitude = Self.normalize(longitude)
-
-        switch Int(normalizedLongitude / 30.0) {
-        case 0: self = .aries
-        case 1: self = .taurus
-        case 2: self = .gemini
-        case 3: self = .cancer
-        case 4: self = .leo
-        case 5: self = .virgo
-        case 6: self = .libra
-        case 7: self = .scorpio
-        case 8: self = .sagittarius
-        case 9: self = .capricorn
-        case 10: self = .aquarius
-        default: self = .pisces
-        }
-    }
-
     var element: ZodiacElement {
         switch self {
         case .aries, .leo, .sagittarius:
@@ -51,11 +32,6 @@ enum ZodiacSign: String, CaseIterable, Codable, Equatable, Hashable {
         case .cancer, .scorpio, .pisces:
             .water
         }
-    }
-
-    private static func normalize(_ longitude: Double) -> Double {
-        let remainder = longitude.truncatingRemainder(dividingBy: 360)
-        return remainder >= 0 ? remainder : remainder + 360
     }
 }
 
